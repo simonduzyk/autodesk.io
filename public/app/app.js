7a9087a6dbf1cda076b574063088d288e10a79a7
wywijam <marek.wywijas@autodesk.com>
2016-07-20 17:17:04 +0200

    front end

--- public/app/app.js
+++ public/app/app.js
@@ -5,15 +5,33 @@ app.service('GameState', function () {
   this.id = '';
   this.state = {};
   var that = this;
+  this.mousePosition = {x:0, y:0};
+  this.center = {x:0, y:0};
+  
+  this.setDraw = function(draw) {
+    this.draw = draw;
+  }
 
   this.onChange = function (state) {
     that.state = state;
+    that.draw();
   }
 
   this.onYourId = function (id) {
     that.id = id;
   }
 
+  this.movePlayer = function() {
+    var dx = that.mousePosition.x - that.center.x;
+    var dy = that.mousePosition.y - that.center.y;
+    var l = Math.sqrt(dx*dx + dy*dy);
+    var dxNorm = dx/l; 
+    var dyNorm = dy/l; 
+    socket.emit('moved', {dx: dxNorm*5, dy: dyNorm*5});
+    setTimeout(that.movePlayer, 25);
+  }
+
+  this.movePlayer();
   socket.on('change', this.onChange);
   socket.on('yourId', this.onYourId);
 });
@@ -23,6 +41,8 @@ app.directive("game", function (GameState) {
     restrict: "A",
     link: function (scope, element) {
       var canvas = element[0];
+      GameState.setDraw(draw);
+      var localCenter = {x: 0, y: 0};
 
       window.onload = showViewport;
       window.onresize = showViewport;
@@ -30,12 +50,18 @@ app.directive("game", function (GameState) {
       var ctx = canvas.getContext('2d');
       var img = new Image();
       img.src = 'assets/background.jpg';
-      backgroundTile = {};
+      backgroundTile = {width:0, height:0};
 
       img.onload = function () {
-        draw();
         backgroundTile.width = this.width;
         backgroundTile.height = this.height;
+        draw();
+      }
+      var getOffset = function(offset) {
+        return {
+          x: offset.x%backgroundTile.width,
+          y: offset.y%backgroundTile.height
+        }
       }
 
       var drawBackground = function (width, height, offset) {
@@ -43,7 +69,7 @@ app.directive("game", function (GameState) {
         var ptrn = ctx.createPattern(img, 'repeat'); // Create a pattern with this image, and set it to "repeat".
         ctx.fillStyle = ptrn;
         ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.translate(offset.x%backgroundTile.width, offset.y%backgroundTile.height);
+        ctx.translate(-offset.x, -offset.y);
         ctx.fillRect(0, 0, width + backgroundTile.width, height + backgroundTile.height); // ctx.fillRect(x, y, width, height);
       }
       function showViewport() {
@@ -52,59 +78,62 @@ app.directive("game", function (GameState) {
         var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         canvas.width = width;
         canvas.height = height;
+        GameState.center = localCenter;
+        draw();
       }
 
-      // variable that decides if something should be drawn on mousemove
-      var drawing = false;
+      // // variable that decides if something should be drawn on mousemove
+      // var drawing = false;
 
-      // the last coordinates before the current move
-      var lastX;
-      var lastY;
+      // // the last coordinates before the current move
 
-      element.bind('mousedown', function (event) {
-        if (event.offsetX !== undefined) {
-          lastX = event.offsetX;
-          lastY = event.offsetY;
-        } else { // Firefox compatibility
-          lastX = event.layerX - event.currentTarget.offsetLeft;
-          lastY = event.layerY - event.currentTarget.offsetTop;
-        }
+      // element.bind('mousedown', function (event) {
+      //   if (event.offsetX !== undefined) {
+      //     lastX = event.offsetX;
+      //     lastY = event.offsetY;
+      //   } else { // Firefox compatibility
+      //     lastX = event.layerX - event.currentTarget.offsetLeft;
+      //     lastY = event.layerY - event.currentTarget.offsetTop;
+      //   }
 
-        // begins new line
-        ctx.beginPath();
+      //   // begins new line
+      //   ctx.beginPath();
 
-        drawing = true;
-      });
+      //   drawing = true;
+      // });
       element.bind('mousemove', function (event) {
-        if (drawing) {
-          // get current mouse position
-          if (event.offsetX !== undefined) {
-            currentX = event.offsetX;
-            currentY = event.offsetY;
-          } else {
-            currentX = event.layerX - event.currentTarget.offsetLeft;
-            currentY = event.layerY - event.currentTarget.offsetTop;
-          }
-
-          draw(lastX, lastY, currentX, currentY);
-
-          // set current coordinates to last one
-          lastX = currentX;
-          lastY = currentY;
+        var currentX, currentY;
+        if (event.offsetX !== undefined) {
+          currentX = event.offsetX;
+          currentY = event.offsetY;
+        } else {
+          currentX = event.layerX - event.currentTarget.offsetLeft;
+          currentY = event.layerY - event.currentTarget.offsetTop;
         }
-
+        GameState.mousePosition = {x: currentX, y: currentY};
       });
-      element.bind('mouseup', function (event) {
-        // stop drawing
-        drawing = false;
-      });
+      // element.bind('mouseup', function (event) {
+      //   // stop drawing
+      //   drawing = false;
+      // });
 
-      // canvas reset
-      function reset() {
-        element[0].width = element[0].width;
+      // // canvas reset
+      // function reset() {
+      //   element[0].width = element[0].width;
+      // }
+
+      function drawUser(centerx, centery, size, color) {
+        ctx.beginPath();
+        ctx.arc(centerx, centery, size, 0, 2 * Math.PI, false);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+
+        ctx.stroke();
       }
 
-      function draw(lX, lY, cX, cY) {
+      function draw() {
 
         var center = { x: 0, y: 0 };
 
@@ -145,19 +174,20 @@ app.directive("game", function (GameState) {
 
         }
 
-        drawBackground(canvas.width, canvas.height, center);
+        var offset = getOffset(center);
+
+        drawBackground(canvas.width, canvas.height, offset);
         // diagnose console
-        ctx.strokeText(GameState.id, 10 - center.x, 10 - center.y);
+        ctx.strokeText(GameState.id, 10 + offset.x, 10 + offset.y);
 
-        ctx.beginPath();
-        ctx.arc(canvas.width / 2 - center.x, canvas.height / 2 - center.y, 20, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'green';
-        ctx.fill();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = '#003300';
-
-        ctx.stroke();
-        setTimeout(draw, 16);
+        localCenter = { x: canvas.width / 2 + offset.x, y: canvas.height / 2 + offset.y};
+        for(var key in GameState.state.players) {
+          var player = GameState.state.players[key];
+          var x = player.coords.x - center.x + localCenter.x;
+          var y = player.coords.y - center.y + localCenter.y;
+          drawUser(x, y, 30, 'green');
+        }
+        //setTimeout(draw, 16);
       }
     }
   };
